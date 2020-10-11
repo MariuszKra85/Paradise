3d9636519082125c27fabdac47fa99a97b112ae0
Mariusz Krawczyk <przyslony@gmail.com>
2020-10-11 13:57:01 +0100

    create hero unit, nav, function to toggle menu class, in css reset style, all header style, and icon style

---
+++ js/menuToggler.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+  let menu = document.querySelector('.navbar-nav');
+  let toggler = document.querySelector('.bars');
+  toggler.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
+})();
