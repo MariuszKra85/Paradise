ac9bf0cacbb6aab35d770b65a6b46cf198015f05
Mariusz Krawczyk <przyslony@gmail.com>
2020-11-16 10:55:27 +0000

    Add map and contact form to contact html

---
+++ js/map.js
@@ -0,0 +1,12 @@
+function initMap() {
+  const leeds = { lat: 53.807, lng: -1.549 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: leeds,
+  });
+
+  const marker = new google.maps.Marker({
+    position: leeds,
+    map: map,
+  });
+}
